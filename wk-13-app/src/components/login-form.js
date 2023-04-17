0c4264ea1a277682ad5713bd19f3438b80778489
Fionna Williams <fswilliams814@icloud.com>
2023-04-17 10:53:11 -0400

    final changes to wk 13 app

--- wk-13-app/src/components/login-form.js
+++ wk-13-app/src/components/login-form.js
@@ -4,11 +4,11 @@ import React from 'react';
 //using this becuase I will be creating multiple elements
 let r = React.createElement;
 
-//export default will allow this class to be accessible outside of the file when webpack compiles everything
+//export default will allow this class to be accessible outside of the file in other JS files
 export default class LoginForm extends React.Component {
     render(){
         return <div className='container'>
-            <div className='col-12'>
+            <div className='container col-sm-12'>
                 <div className='container'>
                 <h3 className='h3'>Log In</h3>
                 <br/>
--- wk-13-app/src/components/nav-bar.js
+++ wk-13-app/src/components/nav-bar.js
@@ -1,20 +1,59 @@
 import React from 'react';
 //import './App.css';
 
-let r = React.createElement
 
-export default class NavBar extends React.Component {
-    render() {
-      return (
-        <div className='col-sm' id='navBar'>
-            <a href='#'>Home</a>
-            <span> </span>
-            <a href='#'>Tranformations</a>
-            <span> </span>
-            <a href='#'>Shop</a>
-        </div>
-      )
+  
+  export default class Navbar extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { showNav: true }; //this will set the state of the showing the Nav to true
+        this.toggleNav = this.toggleNav.bind(this);
     }
+    //this method will all you to click on the navbar toggler icon and switch the state
+    toggleNav() {
+      this.setState({ 
+          showNav: !this.state.showNav
+      })
   }
+  
+  render() {
+      const { showNav } = this.state //sets the nav bar open upon the app starting
+      
+      return (
+      <div className="navbar navbar-expand-lg navbar-light bg-light" id='navBar'>
+          <a className="navbar-brand" href="#">FDB WELLNESS</a>
+          <button className="navbar-toggler" type="button" onClick={this.toggleNav}>
+              <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={(showNav ? 'show' : '') + ' collapse navbar-collapse'}>
+              <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                      <a className="nav-link" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                      <a className="nav-link disabled" href="#">Transformations</a>
+                  </li>
+                  <li className="nav-item">
+                      <a className="nav-link disabled" href="#">Products</a>
+                  </li>
+                  
+                  
+              </ul>
+          </div>
+      </div>
+      );
+  }
+}
 
+// const App = () => {
+// return (
+//    <Navbar />
+// );
+// }
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('app')
+// )
 
